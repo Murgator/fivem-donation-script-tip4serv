c89a8cf8e74c9277403515f49ee8d6963505bb07
Murgator <38978895+Murgator@users.noreply.github.com>
2022-01-10 17:39:36 +0100

    Add files via upload

---
+++ tip4serv/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Murgator'
+description 'Tip4serv'
+version '1.0.5'
+
+shared_script 'config.lua'
+
+server_scripts {
+	'@async/async.lua',
+    'server/sha256.lua',
+    'server/server.lua'
+}
