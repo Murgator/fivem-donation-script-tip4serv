5a3a730a197b8656e25e7bac92c1962e70aa2b23
Murgator <38978895+Murgator@users.noreply.github.com>
2022-01-29 11:05:04 +0100

    Update config.lua

--- tip4serv/config.lua
+++ tip4serv/config.lua
@@ -1,2 +1,7 @@
 Config = {}
-Config.key = "PASTE_YOUR_TIP4SERV_KEY_HERE"
+
+Config.key = "TIP4SERV_KEY" -- paste your tip4serv key here
+
+Config.check_cmd_name = "checkdonate" -- you can edit chat command name. Default: checkdonate
+
+Config.enable_custom_command = false -- set to true if you are using QBCore framework or want to use custom commands: /server/commands.lua
