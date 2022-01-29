8b14aaf477aa56027a76d014d2a05504ab02e7ae
Murgator <38978895+Murgator@users.noreply.github.com>
2022-01-29 11:05:19 +0100

    Update fxmanifest.lua

--- tip4serv/fxmanifest.lua
+++ tip4serv/fxmanifest.lua
@@ -3,11 +3,16 @@ games { 'gta5' }
 
 author 'Murgator'
 description 'Tip4serv'
-version '1.0.6'
+version '2.0.1'
 
 shared_script 'config.lua'
 
+client_scripts {
+    "client/client.lua"
+}
+
 server_scripts {
-    'server/sha256.lua',
-    'server/server.lua'
+    "server/sha256.lua",
+    "server/commands.lua",	
+    "server/server.lua"
 }
