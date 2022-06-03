17e27bc7494e7a2bb976ee59fc1ba9990456e980
Murgator <38978895+Murgator@users.noreply.github.com>
2022-06-03 19:17:15 +0200

    Update config.lua

--- tip4serv/config.lua
+++ tip4serv/config.lua
@@ -2,6 +2,8 @@ Config = {}
 
 Config.key = "TIP4SERV_KEY" -- paste your tip4serv key here
 
+Config.request_interval_in_minutes = 1 -- Check every x minutes if a purchase has been made on Tip4Serv
+
 Config.check_cmd_name = "checkdonate" -- you can edit chat command name. Default: checkdonate
 
 Config.enable_custom_command = false -- set to true if you are using QBCore framework or want to use custom commands: /server/commands.lua
