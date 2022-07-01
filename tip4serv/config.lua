f68e442512287d8a7da792abe5c40b9c43faeeba
Murgator <38978895+Murgator@users.noreply.github.com>
2022-07-01 19:34:34 +0200

    Add text option for translation

--- tip4serv/config.lua
+++ tip4serv/config.lua
@@ -7,3 +7,7 @@ Config.request_interval_in_minutes = 1 -- Check every x minutes if a purchase ha
 Config.check_cmd_name = "checkdonate" -- you can edit chat command name. Default: checkdonate
 
 Config.enable_custom_command = false -- set to true if you are using QBCore framework or want to use custom commands: /server/commands.lua
+
+Config.order_waiting_text = "[Tip4serv] Your order will be delivered within 30 seconds..." -- shown to the player when he types checkdonate command
+
+Config.order_received_text = "[Tip4serv] You have received your order. Thank you !" -- shown to the player when they receive their order
