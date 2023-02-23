e10095b43d2f456b60427a64d9b44eb398c44f29
Murgator <38978895+Murgator@users.noreply.github.com>
2023-02-23 15:47:40 +0100

    Add time_between_each_command option

--- tip4serv/config.lua
+++ tip4serv/config.lua
@@ -4,6 +4,8 @@ Config.key = "TIP4SERV_KEY" -- paste your tip4serv key here
 
 Config.request_interval_in_minutes = 1 -- Check every x minutes if a purchase has been made on Tip4Serv
 
+Config.time_between_each_command = 0 -- Wait x seconds between each command executed
+
 Config.check_cmd_name = "checkdonate" -- you can edit chat command name. Default: checkdonate
 
 Config.enable_custom_command = false -- set to true if you are using QBCore framework or want to use custom commands: /server/commands.lua
