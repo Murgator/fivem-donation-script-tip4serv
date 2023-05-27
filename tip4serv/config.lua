71170e3c71cfb45a83801a7bc8754dda6894750b
Murgator <38978895+Murgator@users.noreply.github.com>
2023-05-27 08:30:23 +0200

    Update config.lua
    added further explanation

--- tip4serv/config.lua
+++ tip4serv/config.lua
@@ -1,15 +1,15 @@
 Config = {}
 
-Config.key = "TIP4SERV_KEY" -- paste your tip4serv key here
+Config.key = "TIP4SERV_KEY" -- Your Tip4Serv server key that you will find here: https://tip4serv.com/dashboard/my-servers
 
 Config.request_interval_in_minutes = 1 -- Check every x minutes if a purchase has been made on Tip4Serv
 
 Config.time_between_each_command = 0 -- Wait x seconds between each command executed
 
-Config.check_cmd_name = "checkdonate" -- you can edit chat command name. Default: checkdonate
+Config.check_cmd_name = "checkdonate" -- You can edit chat command name. Default: checkdonate
 
-Config.enable_custom_command = false -- set to true if you are using QBCore framework or want to use custom commands: /server/commands.lua
+Config.enable_custom_command = false -- Set to true if you are using QBCore framework or want to use custom commands: /server/commands.lua
 
-Config.order_waiting_text = "[Tip4serv] Your order will be delivered within 30 seconds..." -- shown to the player when he types checkdonate command
+Config.order_waiting_text = "[Tip4serv] Your order will be delivered within 30 seconds..." -- Shown to the player when he types checkdonate command
 
-Config.order_received_text = "[Tip4serv] You have received your order. Thank you !" -- shown to the player when they receive their order
+Config.order_received_text = "[Tip4serv] You have received your order. Thank you !" -- Shown to the player when they receive their order
