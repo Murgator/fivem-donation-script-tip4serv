6ce416b21fef2c387a22939c59c29aae5b230b25
Murgator <38978895+Murgator@users.noreply.github.com>
2023-08-08 15:11:29 +0200

    Update server.lua

--- tip4serv/server/server.lua
+++ tip4serv/server/server.lua
@@ -275,17 +275,3 @@ RegisterCommand(Config.check_cmd_name, function(src, args, raw)
         return
     end
 end, false)
-
--- FOR DEBUGGING PURPOSES
-function dump(o)
-    if type(o) == 'table' then
-       local s = '{ '
-       for k,v in pairs(o) do
-          if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
-       end
-       return s .. '} '
-    else
-       return tostring(o)
-    end
-end
