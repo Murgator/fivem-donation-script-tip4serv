e797028bd58f08fc1b0804608151191ce2583181
Murgator <38978895+Murgator@users.noreply.github.com>
2025-01-24 10:52:41 +0100

    Update fxmanifest.lua to 3.0.2

--- tip4serv/fxmanifest.lua
+++ tip4serv/fxmanifest.lua
@@ -1,10 +1,9 @@
 fx_version 'adamant'
-games { 'rdr3', 'gta5' }
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+games { 'gta5' }
 
 author 'Murgator'
 description 'Tip4serv'
-version '3.0.1'
+version '3.0.2'
 
 shared_script 'config.lua'
 
